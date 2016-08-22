caf1ada223d3566d6a621cf0f1938f2aacf897f1
mpedrero <mpedrero10@gmail.com>
2016-08-22 21:15:08 +0200

    New watchface with vertical time Remove dgf.ttf font Add Droid Sans Mono font [BUG] Multiplex TickTimerService to enable updates at different intervals

--- src/mywface.c
+++ src/mywface.c
@@ -3,9 +3,15 @@
 #define MARGIN 3
 #define COLOR_FRONT GColorWhite
 #define COLOR_BACK  GColorBlack
+#define COLOR_DEBUG GColorLightGray
+
+/* Leave for production code. Comment BOTH for debugging */
+#undef APP_LOG
+#define APP_LOG(...) 
 
 static Window* s_main_window;
-static TextLayer* s_time_layer;
+static TextLayer* s_hour_layer;
+static TextLayer* s_minute_layer;
 static TextLayer* s_date_layer;
 static TextLayer* s_step_layer;
 static TextLayer* s_battery_layer;
@@ -13,22 +19,29 @@ static TextLayer* s_bt_layer;
 static GFont s_time_font;
 
 
-/* Function to update the time layer with the current time */
+/* Function to update the time layer with the current time.
+ * TODO: Implement update_hour and update_minute to save battery */
 static void update_time() {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering update_time");
 	/* Get a tm structure */
 	time_t temp = time(NULL);
 	struct tm* tick_time = localtime(&temp);
 
 	/* Write the current hours and minutes into a buffer */
-	static char s_buffer[8];
-	strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+	static char s_hour[8];
+	static char s_minute[8];
+	strftime(s_hour, sizeof(s_hour), clock_is_24h_style() ? "%H" : "%I", tick_time);
+	strftime(s_minute, sizeof(s_minute), "%M", tick_time);
 
 	/* Display this time on the TextLayer */
-	text_layer_set_text(s_time_layer, s_buffer);
+	text_layer_set_text(s_hour_layer, s_hour);
+	text_layer_set_text(s_minute_layer, s_minute);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting update_time");
 }
 
 /* Function to update the date layer with the current date */
 static void update_date() {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering update_date");
 	/* Get a tm structure */
 	time_t temp = time(NULL);
 	struct tm* tick_time = localtime(&temp);
@@ -39,10 +52,13 @@ static void update_date() {
 
 	/* Display date on the TextLayer */
 	text_layer_set_text(s_date_layer, s_date_buffer);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting update_date");
 }
 
-/* Function to update the step layer with the current steps from Pebble Health */
+/* Function to update the step layer with the current steps from Pebble Health.
+ * TODO: Check if Pebble Health is enabled */
 static void update_step() {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering update_step");
 	int step_count;
 	static char s_step_buffer[16];
 
@@ -53,20 +69,23 @@ static void update_step() {
 
 	/* Display steps on the TextLayer */
 	text_layer_set_text(s_step_layer, s_step_buffer);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting update_step");
 }
 
 /* Function called every minute by TickTimerService */
 static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
-	update_time();
-}
-
-/* Function called once a day to update the date */
-static void date_handler(struct tm* tick_date, TimeUnits units_changed) {
-	update_date();
+	if(units_changed & MINUTE_UNIT){
+		update_time();
+		update_step();
+	}
+	if(units_changed & DAY_UNIT){
+		update_date();
+	}
 }
 
 /* Function called when battery state changes */
 static void battery_handler() {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering battery_handler");
 	BatteryChargeState battery_info;
 	static char s_battery[8];
 
@@ -81,10 +100,12 @@ static void battery_handler() {
 
 	/* Display battery status on the TextLayer */
 	text_layer_set_text(s_battery_layer, s_battery);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting battery_handler");
 }
 
 /* Function called when bluetooth connection status changes */
 static void bt_handler(bool connected) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering bt_handler");
 	bool bt_connected;
 	static char s_bt[8];
 
@@ -100,16 +121,20 @@ static void bt_handler(bool connected) {
 
 	/* Display connection status on the TextLayer */
 	text_layer_set_text(s_bt_layer, s_bt);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting bt_handler");
 }
 
 static void main_window_load(Window* window) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering main_window_load");
 	/* Get information about the Window */
 	Layer* window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
 
 	/* Create the TextLayer with specific bounds. Only valid for Pebble/Pebble Time */
-	s_time_layer = text_layer_create(
-		GRect(0, 55, bounds.size.w, 50));
+	s_hour_layer = text_layer_create(
+		GRect(0, 22, bounds.size.w, 64));
+	s_minute_layer = text_layer_create(
+		GRect(0, 74, bounds.size.w, 64));
 	s_date_layer = text_layer_create(
 		GRect(0, 144, bounds.size.w, 24));
 	s_step_layer = text_layer_create(
@@ -120,16 +145,22 @@ static void main_window_load(Window* window) {
 		GRect(bounds.size.w*0.67, 0, bounds.size.w*0.33-MARGIN, 30));
 
 	/* Create GFont */
-	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DGB_48));
+	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SM_62));
 
 	/* Main window */
 	window_set_background_color(window, COLOR_BACK);
 
-	/* Layout for time layer */
-	text_layer_set_background_color(s_time_layer, COLOR_BACK);
-	text_layer_set_text_color(s_time_layer, COLOR_FRONT);
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
-	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	/* Layout for hour layer */
+	text_layer_set_background_color(s_hour_layer, COLOR_BACK);
+	text_layer_set_text_color(s_hour_layer, COLOR_FRONT);
+	text_layer_set_font(s_hour_layer, s_time_font);
+	text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
+
+	/* Layout for minute layer */
+	text_layer_set_background_color(s_minute_layer, COLOR_BACK);
+	text_layer_set_text_color(s_minute_layer, COLOR_FRONT);
+	text_layer_set_font(s_minute_layer, s_time_font);
+	text_layer_set_text_alignment(s_minute_layer, GTextAlignmentCenter);
 
 	/* Layout for date layer */
 	text_layer_set_background_color(s_date_layer, COLOR_BACK);
@@ -155,18 +186,23 @@ static void main_window_load(Window* window) {
 	text_layer_set_font(s_bt_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(s_bt_layer, GTextAlignmentCenter);
 
-	/* Add the layers as a child layers to the Window's root layer */
-	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+	/* Add the layers as a child layers to the Window's root layer. 
+	 * The order here is important to control clipping */
+	layer_add_child(window_layer, text_layer_get_layer(s_minute_layer));
+	layer_add_child(window_layer, text_layer_get_layer(s_hour_layer));
 	layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 	layer_add_child(window_layer, text_layer_get_layer(s_step_layer));
 	layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
 	layer_add_child(window_layer, text_layer_get_layer(s_bt_layer));
 
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting main_window_load");
 }
 
 static void main_window_unload(Window* window) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering main_window_unload");
 	/* Destroy text layers on unload */
-	text_layer_destroy(s_time_layer);
+	text_layer_destroy(s_hour_layer);
+	text_layer_destroy(s_minute_layer);
 	text_layer_destroy(s_date_layer);
 	text_layer_destroy(s_step_layer);
 	text_layer_destroy(s_battery_layer);
@@ -174,14 +210,16 @@ static void main_window_unload(Window* window) {
 
 	/* Destroy text font */
 	fonts_unload_custom_font(s_time_font);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting main_window_unload");
 }
 
 static void init() {
-	/* Register with TickTimerService once a minute to change time */
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering init");
+	/* Register with TickTimerService once a minute to change time. 
+	 * IMPORTANT: Apparently ONLY ONE TickTimerService can be active
+	 * at a time. Newer calls to this function will overwrite this
+	 * listener! */
 	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-
-	/* Register with TickTimerService once a day to change date */
-	tick_timer_service_subscribe(DAY_UNIT, date_handler);
 
 	/* Register with BatteryStateService */
 	battery_state_service_subscribe(battery_handler); 
@@ -209,10 +247,13 @@ static void init() {
 	update_step();
 	battery_handler();
 	bt_handler(connection_service_peek_pebble_app_connection());
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting init");
 }
 
 static void deinit() {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering deinit");
 	window_destroy(s_main_window);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Exiting deinit");
 }
 
 
